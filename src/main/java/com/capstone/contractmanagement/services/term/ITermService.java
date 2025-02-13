97534f99495ba67655e86f9b2d8f130a7b5fd1e8
chinhbean09 <chinh0726@gmail.com>
2025-02-13 10:18:29 +0700

    Convert a function from type term to term

--- src/main/java/com/capstone/contractmanagement/services/term/ITermService.java
+++ src/main/java/com/capstone/contractmanagement/services/term/ITermService.java
@@ -1,15 +1,14 @@
 package com.capstone.contractmanagement.services.term;
 
 import com.capstone.contractmanagement.dtos.term.CreateTermDTO;
-import com.capstone.contractmanagement.dtos.term.UpdateTermDTO;
-import com.capstone.contractmanagement.exceptions.DataNotFoundException;
+import com.capstone.contractmanagement.dtos.term.CreateTypeTermDTO;
+import com.capstone.contractmanagement.entities.TypeTerm;
 import com.capstone.contractmanagement.responses.term.CreateTermResponse;
-
-import java.util.List;
 
 public interface ITermService {
     CreateTermResponse createTerm(Long typeTermId, CreateTermDTO request);
 
+    TypeTerm createTypeTerm(CreateTypeTermDTO request);
 //    CreateTermResponse updateTerm(Long termId, UpdateTermDTO termRequest) throws DataNotFoundException;
 //
 //    List<CreateTermResponse> getAllTerms();
