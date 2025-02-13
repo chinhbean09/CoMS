8e319f0ed3bdc36cc4e1b63d3cd3cddb55be7abd
chinhbean09 <chinh0726@gmail.com>
2025-02-13 17:16:37 +0700

    Add conditions and configure terms for Template

--- src/main/java/com/capstone/contractmanagement/entities/ContractTemplate.java
+++ src/main/java/com/capstone/contractmanagement/entities/ContractTemplate.java
@@ -1,5 +1,6 @@
 package com.capstone.contractmanagement.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -27,13 +28,13 @@ public class ContractTemplate {
     @Column(name = "party_info")
     private String partyInfo;
 
-    //vd: specialTerms cua ben A
+    //specialTerms cua ben A
     @Column(name = "special_termsA")
-    private Long specialTermsA;
+    private String specialTermsA;
 
     //specialTerms cua ben B
     @Column(name = "special_termsB")
-    private Long specialTermsB;
+    private String specialTermsB;
 
     //dc tạo phụ lục
     @Column(name = "appendix_enabled")
@@ -87,11 +88,51 @@ public class ContractTemplate {
     @Column(name = "auto_renew")
     private Boolean autoRenew;
 
+    //  Many-to-Many cho Legal Basis
     @ManyToMany
+    @JsonIgnore
     @JoinTable(
-            name = "contract_template_terms",
+            name = "contract_template_legal_basis",
             joinColumns = @JoinColumn(name = "template_id"),
             inverseJoinColumns = @JoinColumn(name = "term_id")
     )
-    private List<Term> terms = new ArrayList<>();
+    private List<Term> legalBasisTerms = new ArrayList<>();
+
+    // Many-to-Many cho General Terms
+    @ManyToMany
+    @JsonIgnore
+
+    @JoinTable(
+            name = "contract_template_general_terms",
+            joinColumns = @JoinColumn(name = "template_id"),
+            inverseJoinColumns = @JoinColumn(name = "term_id")
+    )
+    private List<Term> generalTerms = new ArrayList<>();
+
+    // Many-to-Many cho Other Terms
+    @ManyToMany
+    @JsonIgnore
+    @JoinTable(
+            name = "contract_template_other_terms",
+            joinColumns = @JoinColumn(name = "template_id"),
+            inverseJoinColumns = @JoinColumn(name = "term_id")
+    )
+    private List<Term> otherTerms = new ArrayList<>();
+
+
+    @ManyToMany
+    @JsonIgnore
+    @JoinTable(
+            name = "contract_template_additional_type_terms", // đổi tên bảng join
+            joinColumns = @JoinColumn(name = "template_id"),
+            inverseJoinColumns = @JoinColumn(name = "term_id")
+    )
+    private List<Term> additionalTerms = new ArrayList<>();
+
+
+    @OneToMany(mappedBy = "contractTemplate", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<ContractTemplateAdditionalTermDetail> additionalTermConfigs = new ArrayList<>();
+
+
 }
