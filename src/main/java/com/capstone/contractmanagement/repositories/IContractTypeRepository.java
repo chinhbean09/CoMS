82a67ee3ab646f99e0098f4022fdc700d70082a9
chinhbean09 <chinh0726@gmail.com>
2025-02-17 15:17:56 +0700

    CRUD for contract type

--- src/main/java/com/capstone/contractmanagement/repositories/IContractTypeRepository.java
+++ src/main/java/com/capstone/contractmanagement/repositories/IContractTypeRepository.java
@@ -1,6 +1,5 @@
 package com.capstone.contractmanagement.repositories;
 
-import com.capstone.contractmanagement.entities.ContractTemplate;
 import com.capstone.contractmanagement.entities.ContractType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
