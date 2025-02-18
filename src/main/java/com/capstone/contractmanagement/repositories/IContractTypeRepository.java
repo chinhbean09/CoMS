0d667805d574bfaccaafbbae5a45f44ed153cbb8
chinhbean09 <chinh0726@gmail.com>
2025-02-18 14:19:39 +0700

    If the type is the same as the one with isDeleted true, it will still be created.

--- src/main/java/com/capstone/contractmanagement/repositories/IContractTypeRepository.java
+++ src/main/java/com/capstone/contractmanagement/repositories/IContractTypeRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IContractTypeRepository extends JpaRepository<ContractType, Long> {
 
     boolean existsByName(String name);
+
+    Optional<ContractType> findByName(String name);
+
 
     List<ContractType> findAllByIsDeletedFalse();
 
