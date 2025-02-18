a09f64a68f7d1ac7fbaca73d68415d9db2925665
chinhbean09 <chinh0726@gmail.com>
2025-02-18 11:05:01 +0700

    try catch all api type contract

--- src/main/java/com/capstone/contractmanagement/repositories/IContractTypeRepository.java
+++ src/main/java/com/capstone/contractmanagement/repositories/IContractTypeRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IContractTypeRepository extends JpaRepository<ContractType, Long> {
+
+    boolean existsByName(String name);
+
 }
