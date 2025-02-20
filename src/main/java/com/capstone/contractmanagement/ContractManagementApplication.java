bfc9b2c1ee80f772d42e3b6ed487a3e7a25b2734
Khang Hoang <khanghoang090703@gmail.com>
2025-02-20 13:05:27 +0700

    fix forgot password

--- src/main/java/com/capstone/contractmanagement/ContractManagementApplication.java
+++ src/main/java/com/capstone/contractmanagement/ContractManagementApplication.java
@@ -2,8 +2,10 @@ package com.capstone.contractmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ContractManagementApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/capstone/contractmanagement/entities/ForgotPassword.java
+++ src/main/java/com/capstone/contractmanagement/entities/ForgotPassword.java
@@ -17,12 +17,22 @@ public class ForgotPassword {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "otp", nullable = false)
     private Integer otp;
+
     @Column(name = "expiration_date", nullable = false)
     private Date expirationTime;
+
     @Column(name = "verified", nullable = false)
     private Boolean verified;
+
+    @Column(name = "reset_token")
+    private String resetToken;  // Token dùng để reset mật khẩu sau khi OTP được xác thực
+
+    @Column(name = "otp_attempts")
+    private int otpAttempts;    // Đếm số lần thử OTP
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
