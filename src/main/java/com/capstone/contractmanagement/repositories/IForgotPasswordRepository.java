7c8f9070d24444128d94cd444efc081f93a5d350
Khang Hoang <khanghoang090703@gmail.com>
2025-02-20 13:05:27 +0700

    fix forgot password

--- src/main/java/com/capstone/contractmanagement/repositories/IForgotPasswordRepository.java
+++ src/main/java/com/capstone/contractmanagement/repositories/IForgotPasswordRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public interface IForgotPasswordRepository extends JpaRepository<ForgotPassword, Long> {
@@ -14,4 +16,15 @@ public interface IForgotPasswordRepository extends JpaRepository<ForgotPassword,
             "( SELECT MAX(fp2.expirationTime) " +
             "FROM ForgotPassword fp2 WHERE fp2.user.email = :email AND fp2.verified = false )")
     Optional<ForgotPassword> findLatestOtpSent(@Param("email") String email);
+
+    @Query("SELECT fp FROM ForgotPassword fp WHERE fp.expirationTime < :now")
+    List<ForgotPassword> findAllExpired(@Param("now") Date now);
+
+    @Query("SELECT fp FROM ForgotPassword fp " +
+            "WHERE fp.user.email = :email " +
+            "AND fp.verified = true " +
+            "AND fp.resetToken = :resetToken " +
+            "ORDER BY fp.expirationTime DESC")
+    Optional<ForgotPassword> findLatestVerifiedOtp(@Param("email") String email,
+                                                   @Param("resetToken") String resetToken);
 }
