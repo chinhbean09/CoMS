ac7776f08a38fdba0d3ff88fddd9efa635a80a46
chinhbean09 <chinh0726@gmail.com>
2025-03-02 15:32:15 +0700

    feature: notification (get-all, mark as read), db migration flyway

---
+++ src/main/java/com/capstone/contractmanagement/responses/notification/NotificationResponse.java
@@ -0,0 +1,15 @@
+package com.capstone.contractmanagement.responses.notification;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class NotificationResponse {
+    private String message;
+    private Boolean isRead;
+    private LocalDateTime createdAt;
+    private Long userId;
+}
