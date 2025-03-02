2c55185c0767df2a0fc06ac5305483a3d1c45455
chinhbean09 <chinh0726@gmail.com>
2025-03-02 15:32:15 +0700

    feature: notification (get-all, mark as read), db migration flyway

---
+++ src/main/java/com/capstone/contractmanagement/entities/Notification.java
@@ -0,0 +1,34 @@
+package com.capstone.contractmanagement.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notifications")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "message", nullable = false, columnDefinition = "TEXT")
+    private String message;
+
+    private Boolean isRead = false;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
