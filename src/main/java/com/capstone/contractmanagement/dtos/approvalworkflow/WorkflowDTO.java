4d1c2fad7ea9bf78ba3f027143d4e327a7132b18
Khang Hoang <khanghoang090703@gmail.com>
2025-03-07 14:20:42 +0700

    approved api, rejected api, fix party entity

---
+++ src/main/java/com/capstone/contractmanagement/dtos/approvalworkflow/WorkflowDTO.java
@@ -0,0 +1,10 @@
+package com.capstone.contractmanagement.dtos.approvalworkflow;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class WorkflowDTO {
+    private String comment;
+}
