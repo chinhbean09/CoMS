1399ca9f55771742f3dd61632ad0b107de407b7d
chinhbean09 <chinh0726@gmail.com>
2025-03-09 14:00:04 +0700

    get user

--- src/main/java/com/capstone/contractmanagement/dtos/user/CreateUserDTO.java
+++ src/main/java/com/capstone/contractmanagement/dtos/user/CreateUserDTO.java
@@ -1,9 +1,14 @@
 package com.capstone.contractmanagement.dtos.user;
 
+import com.capstone.contractmanagement.enums.DepartmentList;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -20,6 +25,11 @@ public class CreateUserDTO {
 
     @JsonProperty("is_ceo")
     private Boolean isCeo;
+
+    @JsonProperty("date_of_birth")
+    private LocalDateTime dateOfBirth;
+
+    private DepartmentList department;
 
     @JsonProperty("role_id")
     private Long roleId;
