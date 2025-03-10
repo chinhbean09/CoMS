6d1f9f147ff1f0187e574a706ddf25ad605866e4
khanghtse <khanghoang090703@gmail.com>
2025-03-10 16:32:59 +0700

    approval workflow

--- src/main/java/com/capstone/contractmanagement/responses/approvalworkflow/ApprovalStageResponse.java
+++ src/main/java/com/capstone/contractmanagement/responses/approvalworkflow/ApprovalStageResponse.java
@@ -1,7 +1,10 @@
 package com.capstone.contractmanagement.responses.approvalworkflow;
 
+import com.capstone.contractmanagement.enums.ApprovalStatus;
 import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -9,4 +12,7 @@ public class ApprovalStageResponse {
     private Long stageId;
     private Integer stageOrder;
     private Long approver;
+    private ApprovalStatus status;
+    private LocalDateTime approvedAt;
+    private String comment;
 }
