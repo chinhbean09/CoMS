3d32b30163e3717f4dc4b7fb70b5eb832608a198
chinhbean09 <chinh0726@gmail.com>
2025-03-10 15:22:57 +0700

    version controll for contract

---
+++ src/main/java/com/capstone/contractmanagement/entities/Addendum.java
@@ -0,0 +1,7 @@
+package com.capstone.contractmanagement.entities;
+
+//Phụ lục là một tài liệu bổ sung, ghi nhận các thay đổi cụ thể trong điều khoản hợp đồng mà không thay thế toàn bộ hợp đồng gốc.
+//Đây là thực tiễn phổ biến trong pháp lý, giúp giữ nguyên tính liên tục của hợp đồng
+//và dễ dàng theo dõi những thay đổi riêng lẻ (ví dụ: "Điều khoản X sửa từ A thành B").
+public class Addendum {
+}
