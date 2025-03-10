bcb2555acfcdac17eeec22dd041e040713571022
khanghtse <khanghoang090703@gmail.com>
2025-03-10 16:32:59 +0700

    approval workflow

--- src/main/java/com/capstone/contractmanagement/repositories/IWorkflowRepository.java
+++ src/main/java/com/capstone/contractmanagement/repositories/IWorkflowRepository.java
@@ -1,7 +1,7 @@
-package com.capstone.contractmanagement.repositories;
-
-import com.capstone.contractmanagement.entities.approval_workflow.Workflow;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IWorkflowRepository extends JpaRepository<Workflow, Long> {
-}
+//package com.capstone.contractmanagement.repositories;
+//
+//import com.capstone.contractmanagement.entities.approval_workflow.Workflow;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface IWorkflowRepository extends JpaRepository<Workflow, Long> {
+//}
