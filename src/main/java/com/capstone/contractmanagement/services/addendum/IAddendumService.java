fa82dcabeceec50b53f17c39c808c9a6442b4cd7
khanghtse <khanghoang090703@gmail.com>
2025-03-12 16:09:14 +0700

    noti for contract, addendum

---
+++ src/main/java/com/capstone/contractmanagement/services/addendum/IAddendumService.java
@@ -0,0 +1,18 @@
+package com.capstone.contractmanagement.services.addendum;
+
+import com.capstone.contractmanagement.dtos.addendum.AddendumDTO;
+import com.capstone.contractmanagement.exceptions.DataNotFoundException;
+import com.capstone.contractmanagement.responses.addendum.AddendumResponse;
+
+import java.util.List;
+
+public interface IAddendumService {
+
+    AddendumResponse createAddendum(AddendumDTO addendumDTO) throws DataNotFoundException;
+
+    List<AddendumResponse> getAllByContractId(Long contractId) throws DataNotFoundException;
+
+    String updateAddendum(Long addendumId, AddendumDTO addendumDTO) throws DataNotFoundException;
+
+    void deleteAddendum(Long addendumId) throws DataNotFoundException;
+}
