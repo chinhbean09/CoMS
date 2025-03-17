6bcc7c690f427eb2f1e2e8b73832e954cb89274b
chinhbean09 <chinh0726@gmail.com>
2025-03-17 17:01:40 +0700

    cannot delete term if it is usage

--- src/main/java/com/capstone/contractmanagement/repositories/ITermRepository.java
+++ src/main/java/com/capstone/contractmanagement/repositories/ITermRepository.java
@@ -74,4 +74,7 @@ public interface ITermRepository extends JpaRepository<Term, Long> {
     @Query(value = "SELECT COUNT(DISTINCT contract_id) FROM contract_terms_snapshot WHERE original_term_id = :termId", nativeQuery = true)
     int countContractUsage(@Param("termId") Long termId);
 
+    @Query("SELECT COUNT(t) FROM ContractTemplate t WHERE :term MEMBER OF t.legalBasisTerms OR :term MEMBER OF t.generalTerms OR :term MEMBER OF t.otherTerms")
+    long countTemplatesUsingTerm(@Param("term") Term term);
+
 }
