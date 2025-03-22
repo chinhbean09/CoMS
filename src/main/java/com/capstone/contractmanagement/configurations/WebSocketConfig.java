be765915c565c4c9a4ccd6dc02cac14a7c5faebe
khanghtse <khanghoang090703@gmail.com>
2025-03-22 20:49:50 +0700

    fix port origin

--- src/main/java/com/capstone/contractmanagement/configurations/WebSocketConfig.java
+++ src/main/java/com/capstone/contractmanagement/configurations/WebSocketConfig.java
@@ -53,7 +53,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://blueedu.info.vn")
+                .setAllowedOriginPatterns("http://blueedu.info.vn", "https://localhost:9999")
                 .withSockJS();
     }
 
