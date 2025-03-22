f334f34289de3e24bf776ffdbded38a3eed6829e
Khang Hoang <khanghoang090703@gmail.com>
2025-03-22 20:29:25 +0700

    fix webscoket

--- src/main/java/com/capstone/contractmanagement/configurations/WebSocketConfig.java
+++ src/main/java/com/capstone/contractmanagement/configurations/WebSocketConfig.java
@@ -53,7 +53,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:9999")
+                .setAllowedOriginPatterns("http://blueedu.info.vn")
                 .withSockJS();
     }
 
