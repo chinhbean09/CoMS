1a71b7b00d2cc7147b8d6c3965820f0f98094c1e
khanghtse <khanghoang090703@gmail.com>
2025-03-28 17:14:09 +0700

    upload payment bill, fix contract partner

--- src/main/java/com/capstone/contractmanagement/controllers/ContractPartnerController.java
+++ src/main/java/com/capstone/contractmanagement/controllers/ContractPartnerController.java
@@ -1,12 +1,15 @@
 package com.capstone.contractmanagement.controllers;
 
 import com.capstone.contractmanagement.dtos.contract_partner.ContractPartnerDTO;
+import com.capstone.contractmanagement.entities.User;
 import com.capstone.contractmanagement.exceptions.DataNotFoundException;
 import com.capstone.contractmanagement.responses.ResponseObject;
+import com.capstone.contractmanagement.responses.User.UserResponse;
 import com.capstone.contractmanagement.services.contract_partner.IContractPartnerService;
 import com.capstone.contractmanagement.utils.MessageKeys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -60,6 +63,17 @@ public class ContractPartnerController {
         return ResponseEntity.ok(ResponseObject.builder()
                 .status(HttpStatus.OK)
                 .message(MessageKeys.UPDATE_CONTRACT_PARTNER_SUCCESSFULLY)
+                .build());
+    }
+
+    @PutMapping(value = "/upload-bill/{paymentScheduleId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ResponseObject> updateUserAvatar(@PathVariable long paymentScheduleId,
+                                                           @RequestParam("file") MultipartFile file) throws DataNotFoundException {
+        contractPartnerService.uploadPaymentBillUrl(paymentScheduleId, file);
+        return ResponseEntity.ok(ResponseObject.builder()
+                .status(HttpStatus.OK)
+                .data(null)
+                .message("Cập nhật đơn thanh toán thành công")
                 .build());
     }
 }
