6b5983409de8239249f61c9986618b5884e13e55
khanghtse <khanghoang090703@gmail.com>
2025-04-02 11:33:59 +0700

    user gender

--- src/main/java/com/capstone/contractmanagement/dtos/user/UpdateUserDTO.java
+++ src/main/java/com/capstone/contractmanagement/dtos/user/UpdateUserDTO.java
@@ -2,6 +2,7 @@ package com.capstone.contractmanagement.dtos.user;
 
 import com.capstone.contractmanagement.entities.Department;
 import com.capstone.contractmanagement.enums.DepartmentList;
+import com.capstone.contractmanagement.enums.GenderList;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
@@ -27,6 +28,8 @@ public class UpdateUserDTO {
 
     private LocalDateTime dateOfBirth;
     private Long departmentId;
+
+    private GenderList gender;
 
     @JsonProperty("role_id")
     private Long roleId;
