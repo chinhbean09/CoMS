2e22ee4deefad28a8b238483b2f7cd1718baf7aa
khanghtse <khanghoang090703@gmail.com>
2025-04-03 16:30:33 +0700

    upload payment image

--- src/main/java/com/capstone/contractmanagement/services/payment/IPaymentScheduleService.java
+++ src/main/java/com/capstone/contractmanagement/services/payment/IPaymentScheduleService.java
@@ -3,8 +3,11 @@ package com.capstone.contractmanagement.services.payment;
 import com.capstone.contractmanagement.dtos.payment.CreatePaymentScheduleDTO;
 import com.capstone.contractmanagement.exceptions.DataNotFoundException;
 
+import java.util.List;
+
 public interface IPaymentScheduleService {
 
     String createPaymentSchedule(Long contractId, CreatePaymentScheduleDTO createPaymentScheduleDTO) throws DataNotFoundException;
     void checkPaymentDue();
+    List<String> getBillUrlsByPaymentId(Long paymentId) throws DataNotFoundException;
 }
