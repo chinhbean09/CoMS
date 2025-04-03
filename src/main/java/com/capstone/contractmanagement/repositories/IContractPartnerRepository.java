8a131ae4b92e1331058fdad60a306bba729ab89f
Khang Hoang <khanghoang090703@gmail.com>
2025-04-03 11:00:19 +0700

    update entity partner contract

--- src/main/java/com/capstone/contractmanagement/repositories/IContractPartnerRepository.java
+++ src/main/java/com/capstone/contractmanagement/repositories/IContractPartnerRepository.java
@@ -1,6 +1,6 @@
 package com.capstone.contractmanagement.repositories;
 
-import com.capstone.contractmanagement.entities.ContractPartner;
+import com.capstone.contractmanagement.entities.PartnerContract;
 import com.capstone.contractmanagement.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,15 +10,15 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface IContractPartnerRepository extends JpaRepository<ContractPartner, Long> {
-    List<ContractPartner> findByUser(User user);
+public interface IContractPartnerRepository extends JpaRepository<PartnerContract, Long> {
+    List<PartnerContract> findByUser(User user);
 
-    @Query("SELECT cp FROM ContractPartner cp " +
+    @Query("SELECT cp FROM PartnerContract cp " +
             "WHERE cp.user = :user AND " +
             "(LOWER(cp.contractNumber) LIKE LOWER(CONCAT('%', :search, '%')) " +
             "OR LOWER(cp.title) LIKE LOWER(CONCAT('%', :search, '%')) " +
             "OR LOWER(cp.partnerName) LIKE LOWER(CONCAT('%', :search, '%')))")
-    Page<ContractPartner> searchByUserAndKeyword(@Param("user") User user,
+    Page<PartnerContract> searchByUserAndKeyword(@Param("user") User user,
                                                  @Param("search") String search,
                                                  Pageable pageable);
 }
