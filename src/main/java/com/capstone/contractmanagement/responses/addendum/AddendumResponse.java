3d6e8c118c0a115c0bda5883934b6a6bee3aa32e
chinhbean09 <chinh0726@gmail.com>
2025-04-14 11:27:44 +0700

    APi get addendum by id

--- src/main/java/com/capstone/contractmanagement/responses/addendum/AddendumResponse.java
+++ src/main/java/com/capstone/contractmanagement/responses/addendum/AddendumResponse.java
@@ -1,12 +1,18 @@
 package com.capstone.contractmanagement.responses.addendum;
 
+import com.capstone.contractmanagement.dtos.contract.ContractItemDTO;
 import com.capstone.contractmanagement.entities.Partner;
 import com.capstone.contractmanagement.entities.contract.ContractPartner;
 import com.capstone.contractmanagement.enums.AddendumStatus;
+import com.capstone.contractmanagement.responses.payment_schedule.PaymentScheduleResponse;
+import com.capstone.contractmanagement.responses.term.TermResponse;
+import com.capstone.contractmanagement.responses.term.TypeTermResponse;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Data
@@ -25,4 +31,15 @@ public class AddendumResponse {
     private Long contractId;
     private Partner partnerA;
     private Optional<ContractPartner> partnerB;
+
+    private List<TermResponse> legalBasisTerms;
+    private List<TermResponse> generalTerms;
+    private List<TermResponse> otherTerms;
+    private List<TypeTermResponse> additionalTerms;
+    private Map<String, Map<String, List<TermResponse>>> additionalConfig;
+
+    // Thông tin thanh toán
+    private List<PaymentScheduleResponse> paymentSchedules;
+    private List<ContractItemDTO> contractItems;
+
 }
