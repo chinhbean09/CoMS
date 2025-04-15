b4364aa4ac7f3b0122a4997f9c6bead7b612fb37
chinhbean09 <chinh0726@gmail.com>
2025-04-15 23:27:19 +0700

    Update addendum dto

--- src/main/java/com/capstone/contractmanagement/dtos/addendum/AddendumTermSnapshotDTO.java
+++ src/main/java/com/capstone/contractmanagement/dtos/addendum/AddendumTermSnapshotDTO.java
@@ -13,7 +13,5 @@ import lombok.NoArgsConstructor;
 public class AddendumTermSnapshotDTO {
     // Đây là ID của term gốc (để tham chiếu nếu cần)
     private Long id;
-    private String label;      // Label của điều khoản
-    private String value;      // Nội dung (value) của điều khoản
 }
 
