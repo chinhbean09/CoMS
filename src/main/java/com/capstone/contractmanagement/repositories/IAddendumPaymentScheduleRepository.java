fd740173097d85d199311431376e5d36f20a74d7
khanghtse <khanghoang090703@gmail.com>
2025-04-19 16:42:10 +0700

    fix update workflow

--- src/main/java/com/capstone/contractmanagement/repositories/IAddendumPaymentScheduleRepository.java
+++ src/main/java/com/capstone/contractmanagement/repositories/IAddendumPaymentScheduleRepository.java
@@ -1,7 +1,17 @@
 package com.capstone.contractmanagement.repositories;
 
 import com.capstone.contractmanagement.entities.addendum.AddendumPaymentSchedule;
+import com.capstone.contractmanagement.entities.contract.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IAddendumPaymentScheduleRepository extends JpaRepository<AddendumPaymentSchedule, Long> {
+    @Query("SELECT aps FROM AddendumPaymentSchedule aps " +
+            "JOIN aps.addendum a " +
+            "WHERE a.contract = :contract " +
+            "AND a.status IN ('APPROVED', 'SIGNED')")
+    List<AddendumPaymentSchedule> findByContractAndApproved(@Param("contract") Contract contract);
 }
