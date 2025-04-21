1f3ae466aa390207909a06863a8380df9af39cb9
chinhbean09 <chinh0726@gmail.com>
2025-04-21 16:43:19 +0700

    Notify for payment in addenda

--- src/main/java/com/capstone/contractmanagement/repositories/IAddendumPaymentScheduleRepository.java
+++ src/main/java/com/capstone/contractmanagement/repositories/IAddendumPaymentScheduleRepository.java
@@ -1,7 +1,9 @@
 package com.capstone.contractmanagement.repositories;
 
+import com.capstone.contractmanagement.entities.addendum.Addendum;
 import com.capstone.contractmanagement.entities.addendum.AddendumPaymentSchedule;
 import com.capstone.contractmanagement.entities.contract.Contract;
+import com.capstone.contractmanagement.enums.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +16,9 @@ public interface IAddendumPaymentScheduleRepository extends JpaRepository<Addend
             "WHERE a.contract = :contract " +
             "AND a.status IN ('APPROVED', 'SIGNED')")
     List<AddendumPaymentSchedule> findByContractAndApproved(@Param("contract") Contract contract);
+
+    @Query("SELECT aps FROM AddendumPaymentSchedule aps WHERE aps.addendum.contract = :contract AND aps.status = :status")
+    List<AddendumPaymentSchedule> findByContractAndStatus(@Param("contract") Contract contract, @Param("status") PaymentStatus status);
+
+
 }
